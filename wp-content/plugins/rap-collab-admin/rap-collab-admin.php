3da78f35e7c320cfd3518ec4de6fa6138f907c4f
dhustlerz <jashan.pahwa007@gmail.com>
2015-10-14 01:51:51 -0400

    update admin panel

--- wp-content/plugins/rap-collab-admin/rap-collab-admin.php
+++ wp-content/plugins/rap-collab-admin/rap-collab-admin.php
@@ -17,6 +17,31 @@ function rap_collab_admin_actions() {
   $output .= '<div class="wrap">';
     $output .= '<h4>Rap Collab Admin</h4>';
   $output .= '</div>';
+
+  $output.='<table class=" widefat datatable table table-striped footer">';
+         $output.='<thead>
+         <tr>
+          <th style="width:20%">YouTube</th>
+          <th style="width:20%">Name</th>
+          <th style="width:20%">IP</th>
+          <th style="width:20%">Likes</th>
+          <th style="width:20%">Status</th>
+        </tr>
+         </thead>';
+
+        $output.='<tbody  class="font-size-12 schedular-speaker-select">';
+        // filter: using meeting.speakerFilter instead of sourcedata.users.speaker.user
+          $output .= '<tr data-ng-repeat="speaker in meeting.speakerFilter" class="">';
+              $output .= '<td style="width:20%">Link</td>';
+              $output .= '<td style="width:20%">test</td>';
+              $output .= '<td style="width:20%">192.168.1.1</td>';
+              $output .= '<td style="width:20%">500</td>';
+              $output .= '<td style="width:20%">approved</td>';
+          $output .= '</tr>';
+        $output.='</tbody>';
+
+      $output.='</table>';
+
   print($output);
  }
 ?>
